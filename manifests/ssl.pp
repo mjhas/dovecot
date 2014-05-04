21b659f4fdaacb2a8222edf1e8aea063be12fdf8
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-05-03 23:51:41 -0700

    Enabling removal of unused settings, generalized ssl
    So we can use 'no', 'yes', or 'required' on ssl, and not hinge the rest
    on it.

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -1,9 +1,10 @@
 class dovecot::ssl (
-  $ssl = 'no',
+  $ssl          = 'no',
   $ssl_certfile = undef,
-  $ssl_keyfile = undef,
-  $ssl_ca = undef,
-) {
+  $ssl_keyfile  = undef,
+  $ssl_ca       = undef,
+  ) {
+
   include dovecot
 
   dovecot::config::dovecotcfsingle { 'ssl':
@@ -11,27 +12,39 @@ class dovecot::ssl (
     value       => $ssl,
   }
 
-  if $ssl == 'yes' {
+  if $ssl_certfile != undef {
     dovecot::config::dovecotcfsingle { 'ssl_cert':
       config_file => 'conf.d/10-ssl.conf',
       value       => "<${ssl_certfile}",
     }
-
-    if $ssl_ca != undef {
-      dovecot::config::dovecotcfsingle { 'ssl_ca':
-        config_file => 'conf.d/10-ssl.conf',
-        value       => "<${ssl_ca}",
-      }
-    } else {
-      dovecot::config::dovecotcfsingle { 'ssl_ca':
-        ensure      => absent,
-        config_file => 'conf.d/10-ssl.conf',
-      }
+  } else {
+    dovecot::config::dovecotcfsingle { 'ssl_cert':
+      ensure      => absent,
+      config_file => 'conf.d/10-ssl.conf',
     }
+  }
 
+  if $ssl_ca != undef {
+    dovecot::config::dovecotcfsingle { 'ssl_ca':
+      config_file => 'conf.d/10-ssl.conf',
+      value       => "<${ssl_ca}",
+    }
+  } else {
+    dovecot::config::dovecotcfsingle { 'ssl_ca':
+      ensure      => absent,
+      config_file => 'conf.d/10-ssl.conf',
+    }
+  }
+
+  if $ssl_keyfile != undef {
     dovecot::config::dovecotcfsingle { 'ssl_key':
       config_file => 'conf.d/10-ssl.conf',
       value       => "<${ssl_keyfile}",
+    }
+  } else {
+    dovecot::config::dovecotcfsingle { 'ssl_key':
+      ensure      => absent,
+      config_file => 'conf.d/10-ssl.conf',
     }
   }
 }
