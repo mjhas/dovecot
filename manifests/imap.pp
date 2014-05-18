d0dab3981afe47cac0f8e768cf99ccdb6f972f68
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-05-18 13:05:15 -0700

    More parameters for IMAP
    - mail_max_userip_connections: set to default of 10
    - imap_idle_notify_interval: set to default of '2 mins'
    
    Using dovecotcfmulti since there are several parameters
    
    Bugfix: mail_plugins should be set inside the 'protocol imap' block

--- manifests/imap.pp
+++ manifests/imap.pp
@@ -1,8 +1,17 @@
-class dovecot::imap ($mail_plugins = '$mail_plugins imap_quota') {
+class dovecot::imap (
+  $mail_plugins                = '$mail_plugins imap_quota',
+  $mail_max_userip_connections = 10,
+  $imap_idle_notify_interval   = '2 mins'
+  ) {
+
   include dovecot
 
-  dovecot::config::dovecotcfsingle { 'mail_plugins':
+  dovecot::config::dovecotcfmulti { 'imap':
     config_file => 'conf.d/20-imap.conf',
-    value       => $mail_plugins,
+    changes     => [
+      "set protocol[ . = \"imap\"]/mail_plugins \"${mail_plugins}\"",
+      "set protocol[ . = \"imap\"]/mail_max_userip_connections \"${mail_max_userip_connections}\"",
+      "set protocol[ . = \"imap\"]/imap_idle_notify_interval \"${imap_idle_notify_interval}\"",
+      ]
   }
 }
