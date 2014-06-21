48ec79e2b413ea99daabc5fb79c44494d4b49d01
hdeadman <hal.deadman@gmail.com>
2014-06-21 12:33:15 -0400

    added some ldap templates and upates for ldap added dovecot::logging class

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -5,7 +5,12 @@ class dovecot::ldap (
   $ldap_auth_bind_userdn = 'cn=%u,cn=users,dc=example,dc=org',
   $ldap_base = 'cn=users,dc=example,dc=org',
   $ldap_auth_bind = 'no',
-  $ldap_scope = 'subtree'
+  $ldap_scope = 'subtree',
+  $ldap_debug_level = '-1',
+  $ldap_user_filter = '(&(objectClass=user)(cn=%u))',
+  $ldap_pass_filter = '(&(objectClass=user)(cn=%u))',
+  $ldap_iterate_attrs =  'cn=user',
+  $ldap_iterate_filter = '(objectClass=user)'
 ) {
   file { "/etc/dovecot/dovecot-ldap.conf.ext":
     ensure  => present,
@@ -15,6 +20,16 @@ class dovecot::ldap (
     group   => dovecot,
     before  => Exec['dovecot'],
   }
+
+  file { "/etc/dovecot/conf.d/auth-ldap.conf.ext":
+    ensure  => present,
+    content => template('dovecot/auth-ldap.conf.ext'),
+    mode    => '0600',
+    owner   => root,
+    group   => dovecot,
+    before  => Exec['dovecot'],
+  }
+
 
   dovecot::config::dovecotcfmulti { 'ldapauth':
     config_file => 'conf.d/10-auth.conf',
