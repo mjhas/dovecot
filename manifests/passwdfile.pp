b92b1c4f5c6d5564238ad6dc29ee86b9466ae520
Tony Thayer <tmt@tonythayer.com>
2014-10-10 13:27:12 -0700

    Create passwdfile.pp
    Creating file. Mimicking postgres auth.

---
+++ manifests/passwdfile.pp
@@ -0,0 +1,20 @@
+class dovecot::passwdfile (
+  $passwdfilename = "/etc/dovecot/dovecot-passdb"
+) {
+  file { "/etc/dovecot/dovecot-sql.conf.ext":
+    ensure  => present,
+    content => template('dovecot/dovecot-passwdfile.conf.ext'),
+    mode    => '0600',
+    owner   => root,
+    before  => Exec['dovecot'],
+  }
+
+  dovecot::config::dovecotcfmulti { 'passwdfileauth':
+    config_file => 'conf.d/10-auth.conf',
+    changes     => [
+      "set include 'auth-passwdfile.conf.ext'",
+      "rm  include[ . = 'auth-system.conf.ext']",
+    ],
+    require => File["/etc/dovecot/dovecot-passwdfile.conf.ext"]
+  }
+}
