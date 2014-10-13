cb0a3502dfe8e49da16ad19c494489f1c7e4e415
Tony Thayer <tmt@tonythayer.com>
2014-10-13 10:47:42 -0700

    Resolved duplicate package declaration error
    Also simplified os lookup for packages.
    
    Comment for mailplugins regarding undef
    
    Fixed messed-up pathing for files and destinations.
    
    Template wasn't generating correctly.
    
    changed <% @whatever -%> to <%= @whatever %>.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,34 +1,19 @@
 class dovecot {
 
-  $imap_package_name = $::osfamily ? {
-    'Debian' => 'dovecot-imapd',
-    'Redhat' => 'dovecot-imapd',
-    default  => 'dovecot-imapd',
+  $mailpackages = $::osfamily ? {
+    default  => ['dovecot-imapd', 'dovecot-pop3d'],
+    'Debian' => ['dovecot-imapd', 'dovecot-pop3d'],
+    'Redhat' => ['dovecot',]
   }
 
-  package { $imap_package_name:
-    ensure => installed,
-    alias  => 'dovecot',
-    before => Exec['dovecot']
-  }
-
-  $pop3_package_name = $::osfamily ? {
-    'Debian' => 'dovecot-pop3d',
-    'Redhat' => 'dovecot',
-    default  => 'dovecot-pop3d',
-  }
-
-  package { $pop3_package_name:
-    ensure => installed,
-    alias  => 'dovecot-pop3d',
-    before => Exec['dovecot']
-  }
+  ensure_packages([$mailpackages])
 
   exec { 'dovecot':
     command     => 'echo "dovecot packages are installed"',
     path        => '/usr/sbin:/bin:/usr/bin:/sbin',
     logoutput   => true,
     refreshonly => true,
+    require     => Package[$mailpackages],
   }
 
   service { 'dovecot':
