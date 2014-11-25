211c32e1b0771339115a449db7c5113355066d25
Michael Haslgrübler <work-michael@haslgruebler.eu>
2014-11-25 21:10:11 +0100

    update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,8 @@ require 'rspec-puppet'
 require 'rspec-puppet-augeas'
 
 RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(__FILE__, '../../../'))
-  puts 'module_path:'+c.module_path
-  c.manifest_dir = File.expand_path(File.join(__FILE__, '..', 'fixtures/manifests'))
-  puts 'manifest_dir:'+c.manifest_dir
-  c.augeas_fixtures = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'augeas')
+   puts 'module_path:'+c.module_path
+   puts 'manifest_dir:'+c.manifest_dir
+   c.augeas_fixtures = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'augeas')
+   puts 'augeas_fixtures:'+c.augeas_fixtures
 end
