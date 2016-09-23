b50d63ae784748d4b2673880a682935305b3c2f9
Angel L. Mateo <amateo@um.es>
2016-09-23 14:43:45 +0200

    Recode of dovecot::imap class
    In order to support all dovecot versions/updates, options are all
    grouped under a "options" Hash. It also provides parameters for:
    * protocol_options: configure options under "protocol imap" in
    20-imap.conf configuration file
    * service_options: configure options under "service imap" in
    10-master.conf configuration file
    * login_options: configure options under "service imap-login" in
    10-master.conf configuration file
    * inet_listeners: configure listeners for imap-login
    
    It also provides dovecot::imap::inet_listeners to create/remove
    listeners for imap-login besides the one define in the inet_listeners
    parameter of dovecot::imap

--- manifests/imap.pp
+++ manifests/imap.pp
@@ -1,17 +1,35 @@
 # 20-imap.conf
+# See README.md for usage
 class dovecot::imap (
-  $mail_plugins                = '$mail_plugins imap_quota',
-  $mail_max_userip_connections = 3,
-  $imap_idle_notify_interval   = '29 mins'
+  Hash[String, Optional[Variant[String,Integer]]] $options          = {},
+  Hash[String, Optional[Variant[String,Integer]]] $protocol_options = {},
+  Hash[String, Optional[Variant[String,Integer]]] $service_options  = {},
+  Hash[String, Optional[Variant[String,Integer]]] $login_options    = {},
+  Hash[String, Hash]                              $inet_listeners   = {},
 ) {
-  include dovecot
+  include ::dovecot
 
-  dovecot::config::dovecotcfmulti { 'imap':
+  ensure_packages('dovecot-imapd')
+
+  dovecot::config::dovecotcfhash {'imap':
     config_file => 'conf.d/20-imap.conf',
-    changes     => [
-      "set protocol[ . = \"imap\"]/mail_plugins \"${mail_plugins}\"",
-      "set protocol[ . = \"imap\"]/mail_max_userip_connections \"${mail_max_userip_connections}\"",
-      "set protocol[ . = \"imap\"]/imap_idle_notify_interval \"${imap_idle_notify_interval}\"",
-      ],
+    options     => merge( $options, prefix($protocol_options, "protocol[. = \"imap\"]/") ),
+  }
+
+  dovecot::master::service {'imap':
+    ensure  => 'present',
+    options => $service_options,
+  }
+
+  dovecot::master::service {'imap-login':
+    ensure  => 'present',
+    options => $login_options,
+  }
+
+  # Configure inet_listeners included in $inet_listeners
+  $inet_listeners.each |String $k, Hash $opt| {
+    dovecot::imap::inet_listener {$k:
+      * => $opt,
+    }
   }
 }
