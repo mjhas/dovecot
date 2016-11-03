eae8a9ed424d3c01654515f1be9993bbccffe201
Angel L. Mateo <amateo@um.es>
2016-11-03 11:20:35 +0100

    Add suports for sieve and managesieve configuration

--- manifests/config/listener.pp
+++ manifests/config/listener.pp
@@ -5,13 +5,14 @@ define dovecot::config::listener (
   Hash $options,
   Enum['present','absent'] $ensure = 'present',
   Enum['unix','inet','fifo'] $type = 'unix',
+  String $config_file = 'conf.d/10-master.conf',
 ) {
   $service_base = "service[ . = \"${service}\"]"
   $listener_base = "${type}_listener[ . = \"${listener_name}\"]"
   $r_options = prefix($options, "${service_base}/${listener_base}/")
   if $ensure == 'present' {
     dovecot::config::dovecotcfmulti {"${listener_name} ${type}_listener ${listener_name}":
-      config_file => 'conf.d/10-master.conf',
+      config_file => $config_file,
       onlyif      => "values ${service_base}/${type}_listener not_include ${listener_name}",
       changes     => [
         "set ${service_base}/${type}_listener[last()+1] ${listener_name}",
@@ -19,13 +20,13 @@ define dovecot::config::listener (
     }
 
     dovecot::config::dovecotcfhash {"${listener_name} ${type}_listener ${listener_name}":
-      config_file => 'conf.d/10-master.conf',
+      config_file => $config_file,
       options     => $r_options,
       require     => Dovecot::Config::Dovecotcfmulti["${listener_name} ${type}_listener ${listener_name}"],
     }
   } else {
     dovecot::config::dovecotcfmulti {"remove ${listener_name} ${type}_listener ${listener_name}":
-      config_file => 'conf.d/10-master.conf',
+      config_file => $config_file,
       onlyif      => "values ${service_base}/${type}_listener include ${listener_name}",
       changes     => [ "rm ${service_base}/${listener_base}" ],
     }
--- manifests/master/service.pp
+++ manifests/master/service.pp
@@ -3,22 +3,23 @@
 define dovecot::master::service (
   Enum['present','absent'] $ensure = 'present',
   Hash[String, Optional[Variant[String,Integer]]] $options = {},
+  String $config_file = 'conf.d/10-master.conf',
 ) {
   if $ensure == 'present' {
     dovecot::config::dovecotcfmulti {"service ${name}":
-      config_file => 'conf.d/10-master.conf',
+      config_file => $config_file,
       onlyif      => "values service not_include ${name}",
       changes     => [ "set service[last()+1] ${name}", ],
     }
 
     dovecot::config::dovecotcfhash {"service ${name} options":
-      config_file => 'conf.d/10-master.conf',
+      config_file => $config_file,
       options     => prefix($options, "service[. = \"${name}\"]/"),
       require     => Dovecot::Config::Dovecotcfmulti["service ${name}"],
     }
   } else {
     dovecot::config::dovecotcfmulti {"remove service ${name}":
-      config_file => 'conf.d/10-master.conf',
+      config_file => $config_file,
       onlyif      => "values service include ${name}",
       changes     => [ "rm service[. = \"${name}\"]", ],
     }
